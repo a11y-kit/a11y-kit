4400b7a1f100f397d35502a96b39044ae262b063
Josh Stabback <josh.stabback@gmail.com>
2020-02-22 20:33:42 -0500

    Fixed rollup config issues

--- packages/vue-tabs/build/rollup.config.js
+++ packages/vue-tabs/build/rollup.config.js
@@ -1,50 +1,54 @@
 // rollup.config.js
-import fs from 'fs';
-import path from 'path';
-import vue from 'rollup-plugin-vue';
-import alias from '@rollup/plugin-alias';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import minimist from 'minimist';
+import fs from 'fs'
+import path from 'path'
+import vue from 'rollup-plugin-vue'
+import alias from '@rollup/plugin-alias'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import minimist from 'minimist'
 
 // Get browserslist config and remove ie from es build targets
-const esbrowserslist = fs.readFileSync('./.browserslistrc')
+const esbrowserslist = fs
+  .readFileSync('./.browserslistrc')
   .toString()
   .split('\n')
-  .filter((entry) => entry && entry.substring(0, 2) !== 'ie');
+  .filter(entry => entry && entry.substring(0, 2) !== 'ie')
 
-const argv = minimist(process.argv.slice(2));
+const argv = minimist(process.argv.slice(2))
 
-const projectRoot = path.resolve(__dirname, '..');
+const projectRoot = path.resolve(__dirname, '..')
 
 const baseConfig = {
   input: 'src/entry.js',
+  treeshake: {
+    moduleSideEffects: false
+  },
   plugins: {
     preVue: [
       replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
+        'process.env.NODE_ENV': JSON.stringify('production')
       }),
       alias({
         resolve: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
         entries: {
-          '@': path.resolve(projectRoot, 'src'),
-        },
-      }),
+          '@': path.resolve(projectRoot, 'src')
+        }
+      })
     ],
     vue: {
       css: true,
       template: {
-        isProduction: true,
-      },
+        isProduction: true
+      }
     },
     babel: {
       exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-    },
-  },
-};
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue']
+    }
+  }
+}
 
 // ESM/UMD/IIFE shared settings: externals
 // Refer to https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
@@ -52,7 +56,8 @@ const external = [
   // list external dependencies, exactly the way it is written in the import statement.
   // eg. 'jquery'
   'vue',
-];
+  '@a11y-kit/utils'
+]
 
 // UMD/IIFE shared settings: output.globals
 // Refer to https://rollupjs.org/guide/en#output-globals for details
@@ -60,10 +65,11 @@ const globals = {
   // Provide global variable names to replace your external imports
   // eg. jquery: '$'
   vue: 'Vue',
-};
+  '@a11y-kit/utils': 'akUtils'
+}
 
 // Customize configs for individual targets
-const buildFormats = [];
+const buildFormats = []
 if (!argv.format || argv.format === 'es') {
   const esConfig = {
     ...baseConfig,
@@ -71,7 +77,7 @@ if (!argv.format || argv.format === 'es') {
     output: {
       file: 'dist/vue-tabs.esm.js',
       format: 'esm',
-      exports: 'named',
+      exports: 'named'
     },
     plugins: [
       ...baseConfig.plugins.preVue,
@@ -82,15 +88,15 @@ if (!argv.format || argv.format === 'es') {
           [
             '@babel/preset-env',
             {
-              targets: esbrowserslist,
-            },
-          ],
-        ],
+              targets: esbrowserslist
+            }
+          ]
+        ]
       }),
-      commonjs(),
-    ],
-  };
-  buildFormats.push(esConfig);
+      commonjs()
+    ]
+  }
+  buildFormats.push(esConfig)
 }
 
 if (!argv.format || argv.format === 'cjs') {
@@ -103,7 +109,7 @@ if (!argv.format || argv.format === 'cjs') {
       format: 'cjs',
       name: 'SampleLib',
       exports: 'named',
-      globals,
+      globals
     },
     plugins: [
       ...baseConfig.plugins.preVue,
@@ -111,14 +117,14 @@ if (!argv.format || argv.format === 'cjs') {
         ...baseConfig.plugins.vue,
         template: {
           ...baseConfig.plugins.vue.template,
-          optimizeSSR: true,
-        },
+          optimizeSSR: true
+        }
       }),
       babel(baseConfig.plugins.babel),
-      commonjs(),
-    ],
-  };
-  buildFormats.push(umdConfig);
+      commonjs()
+    ]
+  }
+  buildFormats.push(umdConfig)
 }
 
 if (!argv.format || argv.format === 'iife') {
@@ -131,7 +137,7 @@ if (!argv.format || argv.format === 'iife') {
       format: 'iife',
       name: 'SampleLib',
       exports: 'named',
-      globals,
+      globals
     },
     plugins: [
       ...baseConfig.plugins.preVue,
@@ -140,13 +146,13 @@ if (!argv.format || argv.format === 'iife') {
       commonjs(),
       terser({
         output: {
-          ecma: 5,
-        },
-      }),
-    ],
-  };
-  buildFormats.push(unpkgConfig);
+          ecma: 5
+        }
+      })
+    ]
+  }
+  buildFormats.push(unpkgConfig)
 }
 
 // Export config
-export default buildFormats;
+export default buildFormats
